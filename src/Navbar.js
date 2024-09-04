6c404f7ccfc0515960da747426d57c51bc62955f
Zach Girouard <zgirouard2@huskers.unl.edu>
2024-09-03 22:23:29 -0500

    transition change

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,44 +15,44 @@ const Navbar = () => {
   return (
 <nav className="navbar">
   <div className="navbar-logo">
-    <HashLink href="#/" className="logo">
+    <a href="#/" className="logo">
       <div className="icon-container">
       <img className="shadow" src={zgShad} alt="Home" />
         <img src={ZGLogo} alt="Home" />
       </div>
-    </HashLink>
+    </a>
   </div>
   <div className="navbar-links">
     <ul className="nav-links">
       <li>
-        <HashLink href="#/">
+        <a href="#/">
         <div className="icon-container">
         <img className="shadow" src={homeShad} alt="Home" />
          <img src={homeIcon} alt="Home" />
          </div>
-        </HashLink>
+        </a>
       </li>
       <li>
       <img className="divider" src={shortLine} alt="divider" />
       </li>
       <li>
-        <HashLink href="#/about">
+        <a href="#/about">
         <div className="icon-container">
         <img className="shadow" src={aboutShad} alt="About" />
         <img src={aboutIcon} alt="About" />
         </div>
-        </HashLink>
+        </a>
       </li>
       <li>
       <img className="divider" src={shortLine} alt="divider" />
       </li>
       <li>
-      <HashLink to="#/about#contactMe">
+      <a to="#/about#contactMe">
         <div className="icon-container">
         <img className="shadow" src={contactShad} alt="Contact" />
         <img src={contactIcon} alt="Contact" />
         </div>
-        </HashLink>
+        </a>
       </li>
     </ul>
   </div>
