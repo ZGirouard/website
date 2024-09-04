16bb0c83090a05bf2f7418f9e69b2d181dad506b
Zach Girouard <zgirouard2@huskers.unl.edu>
2024-09-03 20:42:39 -0500

    router change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 
